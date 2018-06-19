ec6dd511ab85019c123cee7f418c96ef7acdc9c0
Kennedy Morganfield <kennedymorganfield@gmail.com>
2018-06-19 15:12:17 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -16,17 +16,15 @@ def lunch(food="salmon")
   "Time for dinner?  Why not have #{food}?"
 end
 
-
-
-
-
-
 # Call the methods with puts and your own arguments here. Like this:
 puts snacks("Swedish fish")
+
 ## This returns "Any time, is the right time for Sweedish fish!"
 
 # call your methods here
-
+breakfast("oatmeal")
+lunch("salad")
+dinner("pesto pasta")
 
 # Call your methods without any arguments here
 puts snacks
